a478349d288b78d1428e9f5800fe94a6b40fda1b
Isaac <isaacpd@yahoo.com>
2016-12-15 14:15:19 -0500

    Added Point2D.Float constructor

--- src/Effects/Projectile.java
+++ src/Effects/Projectile.java
@@ -12,6 +12,10 @@ public abstract class Projectile extends Rectangle {
 	public Projectile(int x, int y) {
 		dir = new Point2D.Float(x, y);
 	}
+	
+	public Projectile(Point2D.Float p){
+		dir = p;	
+	}
 
 	public void project() {
 		this.x += dir.getX()*speed;
